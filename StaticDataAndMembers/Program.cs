d4a3ecccc945916426ec36867a32ca5699f39c76
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 10:36:40 -0400

    Create SavingsAccount class.

--- StaticDataAndMembers/Program.cs
+++ StaticDataAndMembers/Program.cs
@@ -10,6 +10,11 @@ namespace StaticDataAndMembers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Static Data *****\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            Console.ReadLine();
         }
     }
 }
