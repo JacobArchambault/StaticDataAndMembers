73cf63faed72d34c02258acd3a9501ba4770b2d4
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 10:41:54 -0400

    Add static getters and setters for interest rate

--- StaticDataAndMembers/SavingsAccount.cs
+++ StaticDataAndMembers/SavingsAccount.cs
@@ -14,9 +14,16 @@ namespace StaticDataAndMembers
         // A static point of data.
         public static double currInterestRate = 0.04;
 
+        // A constructor.
         public SavingsAccount(double balance)
         {
             currBalance = balance;
         }
+
+        // Static members to get/set interest rate.
+        public static void SetInterestRate(double newRate)
+        { currInterestRate = newRate; }
+        public static double GetInterestRate()
+        { return currInterestRate; }
     }
 }
